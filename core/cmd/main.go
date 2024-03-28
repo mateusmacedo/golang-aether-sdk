e54e7b77ba08e5397375d586fd16ff1835ec6467
Mateus Macedo Dos Anjos <macedodosanjosmateus@gmail.com>
2024-03-28 08:53:28 -0300

    fix(core): :hammer: optimize bus request handling efficiency

--- core/cmd/main.go
+++ core/cmd/main.go
@@ -38,7 +38,9 @@ func main() {
 
     // Exemplo de requisição e resposta.
     exampleQuery := message.NewExampleQuery() // Supondo que esta função crie uma nova consulta de exemplo.
-    reply, err := bus.RequestReply(context.Background(), exampleQuery, 5*time.Second)
+    ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+    defer cancel()
+    reply, err := bus.RequestReply(ctx, exampleQuery)
     if err != nil {
         fmt.Println("Error requesting reply:", err)
     } else {

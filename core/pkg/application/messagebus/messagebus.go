9cdea80e4a041a87bff4efe3d59fc002242eab83
Mateus Macedo Dos Anjos <macedodosanjosmateus@gmail.com>
2024-03-28 01:22:07 -0300

    feat(messagebus): :sparkles: add message bus interfaces and types

---
+++ core/pkg/application/messagebus/messagebus.go
@@ -0,0 +1,31 @@
+package messagebus
+
+import (
+	"context"
+	"time"
+
+	"github.com/mateusmacedo/golang-aether-sdk/core/pkg/application/message"
+)
+
+// Middleware define a assinatura para um middleware no MessageBus.
+type Middleware func(next HandlerFunc) HandlerFunc
+
+// HandlerFunc define a assinatura para um manipulador de mensagens.
+type HandlerFunc func(ctx context.Context, msg message.Message) error
+
+// MessageBus define a interface para o barramento de mensagens com métodos básicos.
+type MessageBus interface {
+    Publish(ctx context.Context, msg message.Message) error
+    Subscribe(msgType string, handler HandlerFunc) error
+    Unsubscribe(msgType string) error
+}
+
+// CommandBus define a interface para o envio de comandos.
+type CommandBus interface {
+    Send(ctx context.Context, cmd message.Command) error
+}
+
+// QueryBus define a interface para requisições e respostas.
+type QueryBus interface {
+    RequestReply(ctx context.Context, query message.Query, timeout time.Duration) (message.Message, error)
+}

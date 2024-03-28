134c96ea9aefda8aa4caab57a82d806e2ee985aa
Mateus Macedo Dos Anjos <macedodosanjosmateus@gmail.com>
2024-03-28 01:06:10 -0300

    feat(core): :sparkles: add message package and base types

---
+++ core/pkg/application/message/message.go
@@ -0,0 +1,45 @@
+package message
+
+// Message é a interface base para todos os tipos de mensagens.
+type Message interface {
+    Type() string
+}
+
+// Command representa uma instrução para realizar uma ação.
+type Command struct {
+    // Campos específicos do comando
+}
+
+func (c Command) Type() string {
+    return "command"
+}
+
+// Query representa uma solicitação de informação.
+type Query struct {
+    // Campos específicos da consulta
+}
+
+func (q Query) Type() string {
+    return "query"
+}
+
+// Event representa um fato que ocorreu no sistema.
+type Event struct {
+    // Campos específicos do evento
+}
+
+func (e Event) Type() string {
+    return "event"
+}
+
+func NewExampleCommand() Command {
+    return Command{}
+}
+
+func NewExampleQuery() Query {
+    return Query{}
+}
+
+func NewExampleEvent() Event {
+    return Event{}
+}

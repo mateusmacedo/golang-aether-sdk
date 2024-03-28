70387f91bb8abcc9bc6f951ffd7fca57c50f16ba
Mateus Macedo Dos Anjos <macedodosanjosmateus@gmail.com>
2024-03-28 03:41:28 -0300

    feat(core): :sparkles: add main application setup

---
+++ core/cmd/main.go
@@ -0,0 +1,47 @@
+// main.go or a similar setup file
+
+package main
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"github.com/mateusmacedo/golang-aether-sdk/core/pkg/application/message"
+	"github.com/mateusmacedo/golang-aether-sdk/core/pkg/application/messagebus"
+)
+
+func main() {
+    // Criação do GenericMessageBus.
+    bus := messagebus.NewGenericMessageBus()
+
+    // Configuração dos middlewares (se necessário).
+
+    // Exemplo de manipulador para um tipo de mensagem específico.
+    bus.Subscribe("command", func(ctx context.Context, msg message.Message) error {
+        // Lógica de manipulação da mensagem.
+        fmt.Println("Handling Command:", msg)
+        return nil
+    })
+
+    // Exemplo de publicação de uma mensagem.
+    exampleMessage := message.NewExampleEvent() // Supondo que esta função crie uma nova mensagem de exemplo.
+    if err := bus.Publish(context.Background(), exampleMessage); err != nil {
+        fmt.Println("Error publishing message:", err)
+    }
+
+    // Exemplo de envio de um comando.
+    exampleCommand := message.NewExampleCommand() // Supondo que esta função crie um novo comando de exemplo.
+    if err := bus.Send(context.Background(), exampleCommand); err != nil {
+        fmt.Println("Error sending command:", err)
+    }
+
+    // Exemplo de requisição e resposta.
+    exampleQuery := message.NewExampleQuery() // Supondo que esta função crie uma nova consulta de exemplo.
+    reply, err := bus.RequestReply(context.Background(), exampleQuery, 5*time.Second)
+    if err != nil {
+        fmt.Println("Error requesting reply:", err)
+    } else {
+        fmt.Println("Received reply:", reply)
+    }
+}
